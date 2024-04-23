0163b091c96ac43404496327c7df895d4569bae8
BRUNA AMARO <140859773+BRUNAAM@users.noreply.github.com>
2024-04-22 22:25:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+/*MOSTRAR SE UMA VARIAVEL É MAIOR QUE A OUTRA, FAZER UM ALERT E MOSTRAR O RESULTADO NA TELA
+let a=10;
+let b=30
+
+if(a>b){resultado=2*a};
+else(a<b){resultado=a+b};
+alert resultado);
+*/
+
+/*TESTAR UM VALOR DE VARIAVEL E TESTAR SE É NUMERO PRIMO OU NÃO*/
+let a=7;
+const i=0
+resultado=divisivel;
+
+if(a/a){a==resultado;
+
+}
+if(a/1){a==resultado;
+
+}
+else(){}
+
+/*VARIAVEL RECEBE VALOR DE UMA VENDA. SE A VENDA FOR QUE 1.000,00, APLICAR COMISSÃO DE 15%, SE NÃO, 10%*/
